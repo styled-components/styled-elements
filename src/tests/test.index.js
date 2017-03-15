ec8e6f4b568ed84a39e8fd96daef0b17b149c118
Scott Trinh <scott@scotttrinh.com>
2017-03-15 07:19:10 -0400

    test: Add tests for escapeChars

--- src/tests/test.index.js
+++ src/tests/test.index.js
@@ -1,11 +1,23 @@
 // coverage coming soon, just user tested right now
 const assert = require('chai').assert;
 const styled = require('../index.js').default;
+const escapeChars = require('../index.js').escapeChars;
 
 describe('should function normally', () => {
   it('should construct normally', () => {
     assert.equal(typeof styled, 'function');
     assert.equal(typeof styled.div, 'function');
     assert.equal(typeof styled.button, 'function');
+  });
+});
+
+describe('escapeChars', () => {
+  it('removes dangerous characters', () => {
+    assert.equal(escapeChars('&<>"\''), '');
+  });
+
+  it('does not remove non-dangerous characters', () => {
+    const safeString = 'This string contains only safe characters.';
+    assert.equal(escapeChars(safeString), safeString);
   });
 });
